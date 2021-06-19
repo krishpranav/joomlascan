3f286cfa1321b50a4286e0193640dc4ec3d8d6f0
krishpranav <krisna.pranav@gmail.com>
2021-06-19 16:44:22 +0530

    [+] initialize options [+]

--- joomlascan.rb
+++ joomlascan.rb
@@ -24,4 +24,30 @@ class Application
         @use_colours = !opts[:no_colour]
         @target = opts[:url]
     end
+
+    def initialize_options
+        @opts = Slop.parse do |o|
+          o.separator 'Basic options'
+          o.string '-u', '--url', 'The Joomla URL/domain to scan.'
+          o.string '--basic-auth', '<username:password> The basic HTTP authentication credentials'
+          o.bool '-v', '--verbose', 'Enable verbose mode'
+    
+          o.separator 'Enumeration options'
+          o.bool '-a', '--scan-all', 'Scan for all vulnerable extensions'
+          o.bool '-c', '--scan-components', 'Scan for vulnerable components'
+          o.bool '-m', '--scan-modules', 'Scan for vulnerable modules'
+          o.bool '-t', '--scan-templates', 'Scan for vulnerable templates'
+          o.bool '-q', '--quiet', 'Scan using only passive methods'
+    
+          o.separator 'Advanced options'
+          o.bool '--disable-tls-checks', 'Disable SSL/TLS certificate verification.'
+          o.bool '--follow-redirection', 'Automatically follow redirections'
+          o.bool '--no-colour', 'Disable colours in output'
+          o.string '--proxy', '<[protocol://]host:port> HTTP, SOCKS4 SOCKS4A and SOCKS5 are supported. If no protocol is given, HTTP will be used'
+          o.string '--proxy-auth', '<username:password> The proxy authentication credentials'
+          o.integer '--threads', 'The number of threads to use when multi-threading requests', default: 20
+          o.string '--user-agent', 'The user agent string to send with all requests', default: 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
+          o.bool '--hide-banner', 'Do not show the JoomlaVS banner'
+        end
+      end
     
