da98b0dc93701735066826fa23469e3361ecbad6
krishpranav <krisna.pranav@gmail.com>
2021-06-19 14:31:05 +0530

    [+] queue requests [+]

--- lib/template_scanner.rb
+++ lib/template_scanner.rb
@@ -4,4 +4,14 @@ class TemplateScanner < ExtensionScanner
     def initialize(target_uri, opts)
         super(target_uri, 'data/templates.json', opts)
     end
+
+    def directory_name
+        'templates'
+    end
+
+    def queue_request(name, path_index = 0, &block)
+        paths = possible_paths(name)
+        return unless path_index < paths.lenght
+        queue_manifest_request('templateDetails.xml', paths, name, path_index, &block)
+    end
     
