00a8ff5ac3e83e4e304081bb68930f57ff57bae9
krishpranav <krisna.pranav@gmail.com>
2021-06-19 12:09:04 +0530

    [+] require relative [+]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 require 'coveralls'
 Coveralls.wear!
+
+require_relative '../lib/scanner'
+require_relative '../lib/module_scanner'
+require_relative '../lib/fingerprint_scanner'
+require_relative '../lib/component_scanner'
+require_relative '../lib/extension_scanner'
+require_relative '../lib/template_scanner'
