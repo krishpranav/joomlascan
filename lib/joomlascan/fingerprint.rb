91286223bace36e08f1e594eb5967a8213ddcea7
krishpranav <krisna.pranav@gmail.com>
2021-06-19 16:17:59 +0530

    [+] inspect headers [+]

--- lib/joomlascan/fingerprint.rb
+++ lib/joomlascan/fingerprint.rb
@@ -6,4 +6,14 @@ module JoomlaScan
             print_warning("Registration is enabled: #{fingerprint_scanner.target_uri}#{fingerprint_scanner.registration_uri}") if fingerprint_scanner.user_registration_enabled
             print_verbose('User registration is not enabled.') if !fingerprint_scanner.user_registration_enabled
         end
+
+        def inspect_headers
+            print_line_break if opts[:verbose]
+            print_verbose('Looking for intresting headers.....')
+            intresting_headers = fingerprint_scanner.intresting_headers
+            print_good("Found #{intresting_headers.length} intresting headers....")
+            intresting_headers.each do |header|
+                print_indent("#{header[0]}: #{header[1]}")
+            end
+        end
         
