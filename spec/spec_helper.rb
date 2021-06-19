86476290f4397974845acaef978a2d98c4c84f1e
krishpranav <krisna.pranav@gmail.com>
2021-06-19 12:09:42 +0530

    [+] rspec [+]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,9 @@ require_relative '../lib/fingerprint_scanner'
 require_relative '../lib/component_scanner'
 require_relative '../lib/extension_scanner'
 require_relative '../lib/template_scanner'
+
+RSpec.configure do |config|
+    config.before :each do
+        Typhoeus::Expectation.clear
+    end
+end
