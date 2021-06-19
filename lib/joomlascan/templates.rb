a10ce2b4af2b7cc9c7c2a11b8d5c539c832bbe97
krishpranav <krisna.pranav@gmail.com>
2021-06-19 16:35:52 +0530

    [+] templates [+]

--- lib/joomlascan/templates.rb
+++ lib/joomlascan/templates.rb
@@ -3,4 +3,9 @@ module JoomlaScan
         def build_template_filter(scanner)
             scanner.build_filter(fingerprint_scanner.templates_listing_enabled, fingerprint_scanner.administrator_templates_listing_enabled)
         end
-        
+
+        def scan_templates
+            scan(:templates, TemplateScanner, opts[:scan_templates])
+        end
+    end
+end
