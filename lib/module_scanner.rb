936dbc68478fa1d46ab2bbf8464f2c34c021f925
krishpranav <krisna.pranav@gmail.com>
2021-06-19 14:28:46 +0530

    [+] module scanner [+]

--- lib/module_scanner.rb
+++ lib/module_scanner.rb
@@ -1,3 +1,15 @@
 require_relative 'extension_scanner'
 
 class ModuleScanner < ExtensionScanner
+    def initialize(target_uri, opts)
+        super(target_uri, 'data/modules.json', opts)
+    end
+
+    def extension_prefix
+        'mod_'
+    end
+
+    def directory_name
+        'modules'
+    end
+end
