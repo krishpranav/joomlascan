a726fe3e3077bfffa4f933a601b84abf1d7bc91e
krishpranav <krisna.pranav@gmail.com>
2021-06-19 14:38:12 +0530

    [+] calling classes [+]

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -6,4 +6,20 @@ class Scanner
         @opts = opts
         @hydra = Typhoeus::Hydra.new(max_concurrency: opts[:threads])
     end
+
+    def target_uri
+        @target_uri
+    end
+
+    def update_target_uri(value)
+        @target_uri = value.chomp('/')
+    end
+
+    def hydra
+        @hydra
+    end
+
+    def follow_redirection
+        @opts[:follow_redirection]
+    end
     
