cf18576e988a733b87e533a8b6b8fe6bd72e8568
krishpranav <krisna.pranav@gmail.com>
2021-06-19 12:17:41 +0530

    [+] extension scanner initialize [+]

--- lib/extension_scanner.rb
+++ lib/extension_scanner.rb
@@ -2,3 +2,10 @@
 require 'json'
 require 'nokogiri'
 require_relative 'scanner'
+
+class ExtensionScanner < Scanner
+    def initialize(target_uri, data_file, opts)
+        super(target_uri, opts)
+        @data_file = File.join(ExtensionScanner.base_path, data_file)
+    end
+    
