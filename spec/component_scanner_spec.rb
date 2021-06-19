e907a536795cafeaea6dcdf3a31aff7910967a7c
krishpranav <krisna.pranav@gmail.com>
2021-06-19 11:55:08 +0530

    [+] let variables [+]

--- spec/component_scanner_spec.rb
+++ spec/component_scanner_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe ComponentScanner do
     let(:target_uri) { 'http://localhost/' }
     let(:opts_user_agent) { 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0' }
+
+    let(:typhoeus_code) { 200 }
+    let(:typhoeus_body) { '' }
+    let(:typhoeus_headers) { { 'Content-Type' => 'text/html; charset=utf-8' } }
+    
 end
