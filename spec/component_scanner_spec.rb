2e526f8d70db77f20149f8c925691fba8a8bb100
krishpranav <krisna.pranav@gmail.com>
2021-06-19 11:54:19 +0530

    [+] targeturi, user agent [+]

---
+++ spec/component_scanner_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe ComponentScanner do
+    let(:target_uri) { 'http://localhost/' }
+    let(:opts_user_agent) { 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0' }
+end
