d17d8e1a21376d1c747c4a845591726e79f04d51
krishpranav <krisna.pranav@gmail.com>
2021-06-19 16:24:33 +0530

    [+] modules [+]

--- lib/joomlascan/modules.rb
+++ lib/joomlascan/modules.rb
@@ -3,4 +3,9 @@ module JoomlaScan
         def build_modules_filter(scanner)
             scanner.build_filter(fingerprint_scanner.modules_listing_enabled, fingerprint_scanner.administrator_modules_listing_enabled)
         end
-        
+
+        def scan_modules
+            scan(:modules, ModuleScanner, opts[:scan_modules])
+        end
+    end
+end
