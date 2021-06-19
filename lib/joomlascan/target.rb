c8e01f23c345837c1ee99a172c3e86bef88a3670
krishpranav <krisna.pranav@gmail.com>
2021-06-19 16:33:50 +0530

    [+] verify target change [+]

--- lib/joomlascan/target.rb
+++ lib/joomlascan/target.rb
@@ -4,4 +4,14 @@ module JoomlaScan
             fingerprint_scanner.update_target_uri new_uri
             print_verbose("Now targetting #{fingerprint_scanner.target_uri}")
         end
+
+        def verify_target_change(new_uri)
+            print_info("The remote host tried to redirect to: #{new_uri}")
+            answer = read_input('Do you want to follow the redirection?? [Y]es [N]o [A]bort: ')
+            if answer =~ /^y/i
+                update_target_uri(new_uri)
+            elsif answer =~ /^a/i
+                abort_scan
+            end
+        end
         
