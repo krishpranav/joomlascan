b93b110c4f80682a7f380ae567ef4a8d78a09e1c
krishpranav <krisna.pranav@gmail.com>
2021-06-19 16:14:03 +0530

    [+] display vulns [+]

--- lib/joomlascan/extension.rb
+++ lib/joomlascan/extension.rb
@@ -10,4 +10,16 @@ module JoomlaScan
                 print_indent("Reference: #{base_url}#{ref}")
             end
         end
+
+        def display_vulns(vulns)
+            vulns.each do |v|
+              print_line_break
+              print_error("Title: #{v['title']}")
+              display_reference v['edbid'], 'https://www.exploit-db.com/exploits/'
+              display_reference v['cveid'], 'http://www.cvedetails.com/cve/CVE-'
+              display_reference v['url']
+              print_info("Fixed in: #{v['fixed_in']}") if v['fixed_in']
+              print_line_break
+            end
+        end
         
