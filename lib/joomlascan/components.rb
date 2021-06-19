a93698bea4709ea08c40b5af2d4fd3120fcbc530
krishpranav <krisna.pranav@gmail.com>
2021-06-19 16:10:41 +0530

    [+] components [+]

--- lib/joomlascan/components.rb
+++ lib/joomlascan/components.rb
@@ -3,4 +3,10 @@ module JoomlaScan
         def build_components_filter(scanner)
             scanner.build_filter(fingerprint_scanner.components_listing_enabled, fingerprint_scanner.administrator_components_listing_enabled)
         end
-        
+
+        def scan_components
+            scan(:Components, ComponentScanner, opts[:scan_components])
+        end
+    end
+end
+
