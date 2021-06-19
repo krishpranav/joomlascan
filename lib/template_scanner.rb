871d6ec40436197317d554a43c37054cdf29103d
krishpranav <krisna.pranav@gmail.com>
2021-06-19 14:29:58 +0530

    [+] initialize [+]

--- lib/template_scanner.rb
+++ lib/template_scanner.rb
@@ -1,3 +1,7 @@
 require_relative 'extension_scanner'
 
 class TemplateScanner < ExtensionScanner
+    def initialize(target_uri, opts)
+        super(target_uri, 'data/templates.json', opts)
+    end
+    
