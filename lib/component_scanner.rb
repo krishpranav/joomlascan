d77f9762c9e42065b06a98c1673591f3a7d929df
krishpranav <krisna.pranav@gmail.com>
2021-06-19 12:12:51 +0530

    [+] extension prefix [+]

--- lib/component_scanner.rb
+++ lib/component_scanner.rb
@@ -4,4 +4,8 @@ class ComponentScanner < ExtensionScanner
     def initialize(target_uri, opts)
         super(target_uri, 'data/components.json', opts)
     end
+
+    def extension_prefix
+        'com_'
+    end
     
